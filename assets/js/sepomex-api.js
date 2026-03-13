4a33ad22a3197ab0b33adff0727b32c89b3a695e
Victor <victor3790@gmail.com>
2026-03-13 16:43:31 -0600

    States select is now populated with data from the API

--- assets/js/sepomex-api.js
+++ assets/js/sepomex-api.js
@@ -1,4 +1,20 @@
 jQuery(document).ready(function($) {
+    // Populate states
+    $.ajax({
+        url: 'https://sepomex.icalialabs.com/api/v1/states?per_page=32',
+        method: 'GET',
+        success: function(data) {
+            var states = data.states;
+            var $stateSelect = $('#state');
+            states.forEach(function(state) {
+                $stateSelect.append('<option value="' + state.id + '">' + state.name + '</option>');
+            });
+        },
+        error: function(xhr, status, error) {
+            console.log('Error fetching states:', error);
+        }
+    });
+
     $('#zipcode').on('input', function() {
         var zipcode = $(this).val();
         if (zipcode.length === 5) {
